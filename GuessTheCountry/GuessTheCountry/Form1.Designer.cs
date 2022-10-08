6fa73037a3a1a32a2c51315fd06c488e3910a16a
Robert Dinca <71851178+robertDinca2003@users.noreply.github.com>
2022-10-08 12:27:40 +0300

    Add files via upload

---
+++ GuessTheCountry/GuessTheCountry/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace GuessTheCountry
+{
+    partial class MainGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainGame));
+            this.Title = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnStartSurrender = new System.Windows.Forms.Button();
+            this.btnRules = new System.Windows.Forms.Button();
+            this.BestScore = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btn_make = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Title
+            // 
+            this.Title.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Title.AutoSize = true;
+            this.Title.Font = new System.Drawing.Font("Montserrat Black", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Title.Location = new System.Drawing.Point(394, 20);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(434, 55);
+            this.Title.TabIndex = 0;
+            this.Title.Text = "Guess The Country";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(272, 78);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(698, 363);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnStartSurrender
+            // 
+            this.btnStartSurrender.Font = new System.Drawing.Font("Montserrat Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStartSurrender.Location = new System.Drawing.Point(21, 18);
+            this.btnStartSurrender.Name = "btnStartSurrender";
+            this.btnStartSurrender.Size = new System.Drawing.Size(214, 71);
+            this.btnStartSurrender.TabIndex = 2;
+            this.btnStartSurrender.Text = "Start";
+            this.btnStartSurrender.UseVisualStyleBackColor = true;
+            this.btnStartSurrender.Click += new System.EventHandler(this.btnStartSurrender_Click);
+            // 
+            // btnRules
+            // 
+            this.btnRules.Font = new System.Drawing.Font("Montserrat Black", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRules.Location = new System.Drawing.Point(1020, 18);
+            this.btnRules.Name = "btnRules";
+            this.btnRules.Size = new System.Drawing.Size(214, 71);
+            this.btnRules.TabIndex = 3;
+            this.btnRules.Text = "Rules";
+            this.btnRules.UseVisualStyleBackColor = true;
+            this.btnRules.Click += new System.EventHandler(this.btnRules_Click);
+            // 
+            // BestScore
+            // 
+            this.BestScore.AutoSize = true;
+            this.BestScore.Font = new System.Drawing.Font("Montserrat Black", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BestScore.Location = new System.Drawing.Point(32, 616);
+            this.BestScore.Name = "BestScore";
+            this.BestScore.Size = new System.Drawing.Size(269, 27);
+            this.BestScore.TabIndex = 4;
+            this.BestScore.Text = "Your score is: Unknown";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(21, 110);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(214, 164);
+            this.listBox1.TabIndex = 5;
+            // 
+            // btn_make
+            // 
+            this.btn_make.Enabled = false;
+            this.btn_make.Font = new System.Drawing.Font("Montserrat Black", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_make.Location = new System.Drawing.Point(21, 292);
+            this.btn_make.Name = "btn_make";
+            this.btn_make.Size = new System.Drawing.Size(213, 84);
+            this.btn_make.TabIndex = 6;
+            this.btn_make.Text = "Make Guess";
+            this.btn_make.UseVisualStyleBackColor = true;
+            this.btn_make.Click += new System.EventHandler(this.btn_make_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Montserrat Black", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(344, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(551, 132);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "You Won!";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Montserrat Black", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(312, 554);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 27);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Population";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Montserrat Black", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(501, 554);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 27);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Continent";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Montserrat Black", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(683, 554);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(123, 27);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Longitude";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Montserrat Black", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(867, 554);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 27);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Latitude";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Montserrat Black", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(330, 592);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 22);
+            this.label6.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Montserrat Black", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(727, 592);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 22);
+            this.label7.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Montserrat Black", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(911, 592);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 22);
+            this.label8.TabIndex = 14;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Montserrat Black", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(517, 592);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 22);
+            this.label9.TabIndex = 15;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Montserrat Black", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(23, 523);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(212, 30);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "Current Country:";
+            // 
+            // MainGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1258, 664);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_make);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.BestScore);
+            this.Controls.Add(this.btnRules);
+            this.Controls.Add(this.btnStartSurrender);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Title);
+            this.MaximumSize = new System.Drawing.Size(1280, 720);
+            this.MinimumSize = new System.Drawing.Size(1280, 720);
+            this.Name = "MainGame";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Guess The Country";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnStartSurrender;
+        private System.Windows.Forms.Button btnRules;
+        private System.Windows.Forms.Label BestScore;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btn_make;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ GuessTheCountry/GuessTheCountry/Form1.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessTheCountry
+{
+    public partial class MainGame : Form
+    {
+        public MainGame()
+        {
+            InitializeComponent();
+        }
+
+        Dictionary<string, int> ht = new Dictionary<string, int>();
+       public struct Country
+        {
+            public int id;
+            public string name;
+            public float longit;
+            public float latid;
+            public int pop;
+            public string cont;
+        }
+
+        Graphics g;
+        Pen pen;
+
+        bool[] verif = new bool[200];
+
+        Country[] countries = new Country[200];
+
+        int guess = -1;
+
+        int currentGuess = -1;
+
+        int gameStarted = 0;
+
+        int totalGuess = 0;
+
+        public  void init()
+        {
+            if (gameStarted == 1) return;
+            for (int i = 1; i <= 169; i++) verif[i] = false;
+            Random gen = new Random();
+            guess = gen.Next(1, 169);
+
+            btn_make.Enabled = true;
+            btnStartSurrender.Text = "Surrender";
+            ht.Clear();
+            totalGuess = 0;
+            BestScore.Text = "Your score is: Unknown";
+            //MessageBox.Show(Directory.GetCurrentDirectory());
+            string path = Directory.GetCurrentDirectory();
+            path = path += "/TextFile1.txt";
+            var p = @path;
+           // var filePath = @"C:/Users/User/source/repos/GuessTheCountry/GuessTheCountry/Resources/TextFile1.txt";
+            //var filePath = Path.Combine(Directory.GetCurrentDirectory(), "\\TextFile1.txt");
+            
+            using (var streamReader = new StreamReader(p))
+            {
+                string line;
+                while (!string.IsNullOrWhiteSpace(line = streamReader.ReadLine()))
+                {
+                   
+                    int i = 0;
+                    int nr = 0;
+                    
+                    string temp = "";
+                        while(line[i] != ',')
+                        {
+                        nr = nr * 10 + line[i] - 48; i++;
+                        }
+                    i++;
+                    countries[nr].id = nr;
+                    while(line[i] != ',')
+                    {
+                        temp = temp + line[i];
+                        i++;
+                    }
+                    countries[nr].name = temp;
+                    temp = "";
+                    i++;
+                    while (line[i] != ',')
+                    {
+                        if (line[i] != '.')
+                            temp = temp + line[i];
+                        else temp = temp + ',';
+                        i++;
+                    }
+                    countries[nr].latid = float.Parse(temp);
+                    i++;
+                    temp = "";
+                    while (line[i] != ',')
+                    {
+                        if (line[i] != '.')
+                            temp = temp + line[i];
+                        else temp = temp + ',';
+                        i++;
+                    }
+                    countries[nr].longit = float.Parse(temp);
+                    i++;
+                    temp = "";
+                    while (line[i] != ',')
+                    {
+                        temp = temp + line[i];
+                        i++;
+                    }
+                    countries[nr].cont = temp;
+                    i++;
+                    temp = "";
+                    while (line[i] != ',')
+                    {
+                        
+                            temp = temp + line[i];
+                       
+                        i++;
+                    }
+                    countries[nr].pop = int.Parse(temp);
+                    ht.Add(countries[nr].name, countries[nr].id);
+                    listBox1.Items.Add(countries[nr].name);
+                    temp = "";
+                    
+                }
+              
+            }
+            refresh();
+            gameStarted = 1;
+        }
+
+        public void refresh()
+        {
+            listBox1.Items.Clear();
+            
+            for(int i= 1; i <= 169; i++)
+            {
+                if(!verif[i])
+                listBox1.Items.Add(countries[i].name);
+            }
+            if (gameStarted == 0 && listBox1.SelectedIndex != 0) return;
+            g = this.CreateGraphics();
+            g.Clear(Color.White);
+            //pen = new Pen(new SolidBrush(Color.Green));
+            SolidBrush brush;
+
+            if (countries[currentGuess].pop == countries[guess].pop) brush = new SolidBrush(Color.Green);
+            else if(Math.Abs(countries[currentGuess].pop - countries[guess].pop) <= 50000) brush = new SolidBrush(Color.Yellow);
+            else if(Math.Abs(countries[currentGuess].pop - countries[guess].pop) <= 500000) brush = new SolidBrush(Color.Orange);
+            else brush = new SolidBrush(Color.Red);
+
+            g.FillEllipse(brush,220,290,65,65);
+
+            PointF[] pointFs = new PointF[3];
+            if (countries[currentGuess].pop > countries[guess].pop)
+            {
+                pointFs[0] = new Point(232, 315);
+                pointFs[1] = new Point(272, 315);
+                pointFs[2] = new Point(252, 345);
+            }
+            else
+            {
+                pointFs[0] = new Point(232, 335);
+                pointFs[1] = new Point(272, 335);
+                pointFs[2] = new Point(252, 305);
+            }
+
+            if(countries[currentGuess].pop != countries[guess].pop)
+            g.FillPolygon(new SolidBrush(Color.AntiqueWhite), pointFs);
+            label6.Text = countries[currentGuess].pop.ToString();
+            
+            if(countries[currentGuess].cont == countries[guess].cont)
+                g.FillEllipse(new SolidBrush(Color.Green), 340, 290, 65, 65);
+            else
+                g.FillEllipse(new SolidBrush(Color.Red), 340, 290, 65, 65);
+
+            label9.Text = countries[currentGuess].cont;
+            
+
+            if(countries[currentGuess].longit == countries[guess].longit)
+            {
+                g.FillEllipse(new SolidBrush(Color.Green), 460, 290, 65, 65);
+            }
+            else if (Math.Abs(countries[currentGuess].longit-countries[guess].longit) <= 10) g.FillEllipse(new SolidBrush(Color.Yellow), 460, 290, 65, 65);
+            else if (Math.Abs(countries[currentGuess].longit - countries[guess].longit) <= 35) g.FillEllipse(new SolidBrush(Color.Orange), 460, 290, 65, 65);
+            else g.FillEllipse(new SolidBrush(Color.Red), 460, 290, 65, 65);
+
+            if(countries[currentGuess].longit > countries[guess].longit)
+            {
+                pointFs[0] = new Point(472, 325);
+                pointFs[1] = new Point(502, 310);
+                pointFs[2] = new Point(502, 340);
+            }
+            else if(countries[currentGuess].longit < countries[guess].longit)
+            {
+                pointFs[0] = new Point(512, 325);
+                pointFs[1] = new Point(482, 310);
+                pointFs[2] = new Point(482, 340);
+            }
+            if(currentGuess != guess)
+            g.FillPolygon(new SolidBrush(Color.AntiqueWhite), pointFs);
+            label7.Text = countries[currentGuess].longit.ToString(); 
+
+
+            if(countries[currentGuess].latid == countries[guess].latid)
+                g.FillEllipse(new SolidBrush(Color.Green), 580, 290, 65, 65);
+            else if(Math.Abs(countries[currentGuess].latid - countries[guess].latid) <= 10) g.FillEllipse(new SolidBrush(Color.Yellow), 580, 290, 65, 65);
+            else if (Math.Abs(countries[currentGuess].latid - countries[guess].latid) <= 35) g.FillEllipse(new SolidBrush(Color.Orange), 580, 290, 65, 65);
+            else  g.FillEllipse(new SolidBrush(Color.Red), 580, 290, 65, 65);
+
+            if (countries[currentGuess].latid > countries[guess].latid)
+            {
+                pointFs[0] = new Point(592, 315);
+                pointFs[1] = new Point(632, 315);
+                pointFs[2] = new Point(612, 345);
+            }
+            else if (countries[currentGuess].latid < countries[guess].latid)
+            {
+                pointFs[0] = new Point(592, 335);
+                pointFs[1] = new Point(632, 335);
+                pointFs[2] = new Point(612, 305);
+            }
+
+            if (currentGuess != guess)
+                g.FillPolygon(new SolidBrush(Color.AntiqueWhite), pointFs);
+
+            label8.Text = countries[currentGuess].latid.ToString();
+
+            label10.Text = countries[currentGuess].name;
+        }
+
+        public void checkGuess(int id)
+        {
+            if (id == guess) winner();
+            else if (totalGuess >= 15) loser();
+            else refresh();
+        }
+
+        public void winner()
+        {
+            btn_make.Enabled = false;
+            btnStartSurrender.Text = "New Game";
+            pictureBox1.Visible = false;
+            label1.Text = "You Won!";
+            label1.Visible = true;
+            refresh();
+        }
+
+        public void loser()
+        {
+
+            label1.Text = "You Lose!";
+            btn_make.Enabled = false;
+            pictureBox1.Visible = false;
+            label1.Visible = true;
+            if(totalGuess != 0)
+            refresh();
+            btnStartSurrender.Text = "New Game";
+        }
+
+        private void btnStartSurrender_Click(object sender, EventArgs e)
+        {
+            if(btnStartSurrender.Text == "Start")
+            init();
+            else if(btnStartSurrender.Text == "Surrender")
+            {
+                loser();
+            }
+            else
+            {
+                gameStarted = 0;
+                btnStartSurrender.Text = "Start";
+                label1.Visible = false;
+                pictureBox1.Visible = true;
+                btn_make.Enabled = false;
+                if(totalGuess!=0)
+                g.Clear(Color.White);
+                label6.Text = "";
+                label7.Text = "";
+                label8.Text = "";
+                label9.Text = "";
+                label10.Text = "Current Country:";
+            }
+        }
+
+        private void btn_make_Click(object sender, EventArgs e)
+        {
+            totalGuess++;
+            BestScore.Text = "Your score is: " + totalGuess.ToString();
+            currentGuess = ht[listBox1.SelectedItem.ToString()];
+            verif[currentGuess] = true;
+            checkGuess(currentGuess);
+        }
+
+        private void btnRules_Click(object sender, EventArgs e)
+        {
+            if(gameStarted == 1)
+            MessageBox.Show(countries[guess].name);
+        }
+    }
+}
